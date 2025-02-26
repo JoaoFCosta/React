4830c060d8f490ee3c5cd00a5945957961fee91e
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-02-26 13:50:26 -0300

    Finalizando a estrutura React

--- 03devlinks/src/App.jsx
+++ 03devlinks/src/App.jsx
@@ -1,31 +1,48 @@
 import "./App.css";
+import foto from "./img/OIP.jpg";
+
 import Perfil from "./components/perfil/perfil";
 import Switch from "./components/switch/Switch";
 import Links from "./components/links/Links";
 import SocialLinks from "./components/socialLinks/SocialLinks";
 import Rodape from "./components/rodape/Rodape";
+import { useState } from "react";
 
 const App = () => {
+  const [isLight, setIsLight] = useState(true);
+
+  const troca = () => {
+    setIsLight(!isLight);
+  };
+
   return (
-    <div id="App" className="">
-      <Perfil />
-      <Switch />
+    <div id="App" className={isLight ? "light" : ""}>
+      <Perfil fotoPerfil={foto}>João</Perfil>
+
+      <Switch troca={troca} isLight={isLight}/>
 
       <ul>
-        <Links />
-        <Links />
-        <Links />
-        <Links />
+        <Links link={"https://github.com/JoaoFCosta"}>Github</Links>
+        <Links link={"https://github.com/JoaoFCosta?tab=repositories"}>
+          Portfólio
+        </Links>
+        <Links link={"https://instagram.com"}>Instagram</Links>
+        <Links link={"https://github.com/JoaoFCosta?tab=repositories"}>
+          Projetos
+        </Links>
       </ul>
 
       <div id="socialLinks">
-        <SocialLinks />
-        <SocialLinks />
-        <SocialLinks />
-        <SocialLinks />
+        <SocialLinks
+          link={"https://github.com/JoaoFCosta"}
+          icon={"logo-github"}
+        />
+        <SocialLinks link={"https://instagram.com"} icon={"logo-instagram"} />
+        <SocialLinks link={"https://youtube.com"} icon={"logo-youtube"} />
+        <SocialLinks link={"https://linkedin.com"} icon={"logo-linkedin"} />
       </div>
 
-      <Rodape />
+      <Rodape>João</Rodape>
     </div>
   );
 };
