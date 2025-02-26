53893142604f72bada210d47d0be1f199928a5b3
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-02-26 08:06:19 -0300

    first commit

---
+++ 03devlinks/src/components/rodape/Rodape.jsx
@@ -0,0 +1,13 @@
+import styles from "./Rodape.module.css";
+
+const Rodape = () => {
+  return (
+    <footer>
+      <p>
+        Feito com 🤍 por <a href="https://github.com">Nome</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
